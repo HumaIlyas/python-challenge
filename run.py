c69ebb52e5dd576a7ac3dbabe9b93e3a7d4bfceb
HumaIlyas <hi.wtmconsult@gmail.com>
2023-01-07 10:47:43 +0000

    save file

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+## zero_after_one, count how many zeros in the list are after 1
+
+def zero_after_one(list):
+  counter = 0
+  start = list.i(1)
+  list = []
+
+  for zero in list:
+    if i == 0:
+        i += 1
+    else:
+        break
+  return counter
+
+
+assert zero_after_one([]) == 0
+assert zero_after_one([1, 2, 3]) == 0
+assert zero_after_one([0, 1, 2]) == 0
+assert zero_after_one([0, 0, 0, 0]) == 0
+assert zero_after_one([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) == 0
+assert zero_after_one([1, 0]) == 1
+assert zero_after_one([0, 1, 0]) == 1
+assert zero_after_one([0, 1, 0, 0, 0, 0, 0]) == 5
+
+print("Everything is correct")
+
